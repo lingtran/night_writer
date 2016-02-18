c7fff608c8e7e12c7a227977f44e113f91ce8d80
lingtran <lxiutran@gmail.com>
2016-02-17 21:08:29 -0700

    edited test to reflect renaming of row method to row_at

--- test/night_write_test.rb
+++ test/night_write_test.rb
@@ -16,29 +16,29 @@ class NightWriteTest < Minitest::Test
   def test_single_lowercase_word_can_be_printed_in_braille
     braille = NightWriter.new
 
-    assert_equal "0..0", braille.row(0,"hi")
-    assert_equal "000.", braille.row(1,"hi")
-    assert_equal "....", braille.row(2,"hi")
+    assert_equal "0..0", braille.row_at(0,"hi")
+    assert_equal "000.", braille.row_at(1,"hi")
+    assert_equal "....", braille.row_at(2,"hi")
   end
 
   def test_single_capitalized_word_can_be_printed_in_braille
     braille = NightWriter.new
 
-    assert_equal "..0..0", braille.row(0,"Hi")
-    assert_equal "..000.", braille.row(1,"Hi")
-    assert_equal ".0....", braille.row(2,"Hi")
+    assert_equal "..0..0", braille.row_at(0,"Hi")
+    assert_equal "..000.", braille.row_at(1,"Hi")
+    assert_equal ".0....", braille.row_at(2,"Hi")
   end
 
   def test_white_spaces_can_be_included_in_braille
     braille = NightWriter.new
 
-    assert_equal "..0..0.....00.0.0.00", braille.row(0,"Hi World")
-    assert_equal "..000.....00.0000..0", braille.row(1,"Hi World")
-    assert_equal ".0.......0.00.0.0...", braille.row(2,"Hi World")
+    assert_equal "..0..0.....00.0.0.00", braille.row_at(0,"Hi World")
+    assert_equal "..000.....00.0000..0", braille.row_at(1,"Hi World")
+    assert_equal ".0.......0.00.0.0...", braille.row_at(2,"Hi World")
 
-    assert_equal "..0..0.....00.0.0.00....000.0.", braille.row(0, "Hi World You")
-    assert_equal "..000.....00.0000..0.....0.0..", braille.row(1, "Hi World You")
-    assert_equal ".0.......0.00.0.0......0000.00", braille.row(2, "Hi World You")
+    assert_equal "..0..0.....00.0.0.00....000.0.", braille.row_at(0, "Hi World You")
+    assert_equal "..000.....00.0000..0.....0.0..", braille.row_at(1, "Hi World You")
+    assert_equal ".0.......0.00.0.0......0000.00", braille.row_at(2, "Hi World You")
   end
 
   def test_numbers_can_be_printed_in_braille
