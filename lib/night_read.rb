aa03e300be20198f9e177ae635fa5015931f4fdf
lingtran <lxiutran@gmail.com>
2016-02-17 19:22:07 -0700

    tranpose method method added and validated

--- lib/night_read.rb
+++ lib/night_read.rb
@@ -11,15 +11,23 @@ class NightReader
   end
 
   def decode_to_original_message(string)
-
+    # invert to extract
+    transpose(string).
   end
 
   def parser(number, string)
-    string.split[number].chars.each_with_index { |char, index| index == 0 ? char : index.even? ? char.prepend(" ") : char }.join.split(" ")
+    string.split[number].chars.each_with_index { |char, index| index == 0 ? char : index.even? ? char.prepend(" ") : char }.join.split
+
+    # perhaps put this method and transpose in reader module?
   end
 
-  def row(number, string)
+  def transpose(string)
+    top_row = parser(0, string)
+    middle_row = parser(1, string)
+    bottom_row = parser(2, string)
 
+    aggregate_row = Array.new
+    aggregate_row.push(top_row, middle_row, bottom_row).transpose
   end
 
   def decode_file_to_original_message(string)
